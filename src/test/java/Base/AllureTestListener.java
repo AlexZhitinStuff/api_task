b434c3b766ec5fa9ac3de5d61742b1d6580cb2b4
aszhitin <aszhitin@gmail.com>
2020-07-05 11:08:39 +0300

    Added AllureTestListener

--- src/test/java/Base/AllureTestListener.java
+++ src/test/java/Base/AllureTestListener.java
@@ -1,26 +1,11 @@
 package Base;
 
-import io.qameta.allure.Attachment;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 
 public class AllureTestListener implements ITestListener {
-
-
-    @Attachment(value = "Page screenshot", type = "image/png")
-    public byte[] saveScreenshotPNG(WebDriver driver) {
-        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-    }
-
-    @Attachment(value = "{0}", type = "text/plain")
-    public static String saveTextLog(String message) {
-        return message;
-    }
 
 
     public void onStart(ITestContext context) {
@@ -45,12 +30,7 @@ public class AllureTestListener implements ITestListener {
     @Override
     public void onTestFailure(ITestResult result) {
         System.out.println("*** Execution of a test method" + result.getMethod().getMethodName() + " of a test class " + result.getMethod().getRealClass().getName() + " failed...");
-        Object testClass = result.getInstance();
-        WebDriver webDriver = ((TestBase) testClass).getDriver();
 
-        System.out.println("Screenshot captured for test case:" + (result.getMethod().getMethodName()));
-        saveScreenshotPNG(webDriver);
-        saveTextLog(result.getMethod().getMethodName() + " failed and screenshot taken!");
     }
 
     @Override
